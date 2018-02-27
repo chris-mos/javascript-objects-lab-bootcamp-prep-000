7a78030ab4c5247c6da761e3c66384e198f57a14
Christopher Moser <moser121@icloud.com>
2018-02-27 20:09:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var recipes = {cake:'flour'}
 function updateObjectWithKeyAndValue(object,key,value){
   return Object.assign({},object,{[key]:value})
 }
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  return object[key] = value
+}
